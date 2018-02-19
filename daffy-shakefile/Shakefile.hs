782729d13406bb0fcaa5efec0a0d50806547e34c
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-02-19 01:52:53 -0500

    More shakefile improvements

--- daffy-shakefile/Shakefile.hs
+++ daffy-shakefile/Shakefile.hs
@@ -1,43 +1,39 @@
-{-# language LambdaCase #-}
-{-# language RankNTypes #-}
+{-# language LambdaCase   #-}
+{-# language RankNTypes   #-}
+{-# language ViewPatterns #-}
 
 import Control.Concurrent (newChan, readChan)
-import Data.Function ((&))
-import Data.HashMap.Strict (HashMap)
-import Data.Text (Text, pack)
-import Data.Text.Encoding (encodeUtf8)
+import Control.Exception (SomeAsyncException(SomeAsyncException), fromException, throwIO, try)
+import Data.Functor (void)
 import Development.Shake
-import Development.Shake.FilePath
-import System.Directory (getCurrentDirectory)
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory)
 import System.Environment (getArgs, getEnvironment, withArgs)
 import System.FSNotify (eventPath, watchTreeChan, withManager)
 import System.Posix.Process (executeFile)
-import UnliftIO (tryAny)
 
-import qualified Data.Aeson as Aeson (Value, json, withObject)
-import qualified Data.Aeson.Internal as Aeson (iparse)
-import qualified Data.Aeson.Parser as Aeson (eitherDecodeStrictWith)
-import qualified Data.Aeson.Types as Aeson (Parser, parseField)
-import qualified Data.HashMap.Strict as HashMap (toList)
 import qualified Data.Set as Set (fromList)
 
 main :: IO ()
 main =
   getArgs >>= \case
-    ["watch"] ->
-      withArgs [] $ do
-        _ <- tryAny run
+    "watch":args ->
+      withArgs args $ do
+        -- Run 'shake' under a 'try', ignoring synchronous exceptions because
+        -- we don't care if it fails.
+        try run >>= \case
+          Left (fromException -> Just (SomeAsyncException ex)) -> throwIO ex
+          _ -> pure ()
+
+        -- Start watching the filesystem, and rebuild once some file that shake
+        -- considered alive changes (it writes these files to .shake/live).
         cwd <- getCurrentDirectory
-        files <-
-          Set.fromList . map ((cwd ++) . ('/':)) . lines
-            <$> readFile ".shake/live"
-        withManager $ \mgr -> do
+        files <- Set.fromList . map ((cwd ++) . ('/':)) . lines <$> readFile ".shake/live"
+        withManager $ \manager -> do
           chan <- newChan
-          _ <- watchTreeChan mgr "." ((`elem` files) . eventPath) chan
-          _ <- readChan chan
-          pure ()
+          void (watchTreeChan manager "." ((`elem` files) . eventPath) chan)
+          void (readChan chan)
         env <- getEnvironment
-        executeFile "bin/Shakefile" False ["watch"] (Just env)
+        executeFile "bin/Shakefile" False ("watch":args) (Just env)
     _ -> do
       run
  where
@@ -61,57 +57,62 @@ rules :: (forall a. Action a -> Action a) -> Rules ()
 rules stack = do
   -- The files we want to build, in no particular order.
   want
-    [ ".shake/gitmodules"
+    [ ".shake/.gitmodules"
     , "bin/daffy"
     , "bin/Shakefile"
     ]
 
   phony "clean" $ do
-    liftIO (removeFiles "bin" ["*"])
-    removeFilesAfter ".shake" ["*"]
+    liftIO (removeFiles "bin" ["//"])
+    liftIO (removeFiles "daffy-client/codegen" ["//"])
+    liftIO (removeFiles "daffy-server/codegen" ["//"])
+    removeFilesAfter ".shake" ["//"]
+
+  phony "deepclean" $ do
+    need ["clean"]
+    liftIO (removeFiles ".stack-work" ["//*"])
+    liftIO (removeFiles "elm-stuff" ["//*"])
 
   -- Whenever .gitmodules changes, recursively init/update all of them.
-  ".shake/gitmodules" %> \out -> do
-    Stdout xs <- cmd "git config -f .gitmodules --get-regexp path$"
-    let submodules = xs & lines & map ((</> ".git") . (!! 1) . words)
-    need (".gitmodules" : submodules)
+  ".shake/.gitmodules" %> \out -> do
+    need [".gitmodules"]
     cmd_ "git submodule update --init --recursive"
     writeFile' out ""
 
-  -- Snip out just the dependencies from "elm-package.json" and write them to
-  -- ".shake/elm-deps.txt".
-  ".shake/elm-deps.txt" %> \out -> do
-    bytes <- encodeUtf8 . pack <$> readFile' "elm-package.json"
-    let parser :: Aeson.Value -> Aeson.Parser (HashMap Text Text)
-        parser =
-          Aeson.withObject "elm-package.json" $ \o ->
-            Aeson.parseField o (pack "dependencies")
-    case Aeson.eitherDecodeStrictWith Aeson.json (Aeson.iparse parser) bytes of
-      Left (_, err) ->
-        fail err
-      Right deps ->
-        writeFileChanged out (show (HashMap.toList deps))
-
-  -- Write an empty "elm-deps.stamp" file whenever the dependencies in
-  -- "elm-package.json" change.
-  ".shake/elm-deps.stamp" %> \out -> do
-    need [".shake/elm-deps.txt"]
-    cmd_ "elm package install --yes"
-    writeFile' out ""
+  -- Whenever elm-package.json changes, run "elm package install --yes". If
+  -- something actually happened, update the modtime of .shake/elm-package.json.
+  ".shake/elm-package.json" %> \out -> do
+    need ["elm-package.json"]
+    Stdout output <- cmd "elm package install --yes"
+    case output of
+      "Packages configured successfully!" -> pure ()
+      _ -> writeFile' out ""
 
   "bin/daffy" %> \_ -> do
+    -- Require "daffy.js" to be built before building the server, but don't
+    -- actually add it as a dependency, because (in development mode) we don't
+    -- need to rebuild the server if "daffy.js" changes.
+    doesFileExist "daffy-server/codegen/daffy.js" >>= \case
+      False -> do
+        -- Touch the file because in order to build "daffy.js", we need to build
+        -- and run 'daffy-elm-codegen', which will build 'daffy' the library,
+        -- which will require "daffy.js" to exist (it's a data-file).
+        liftIO (createDirectoryIfMissing False "daffy-server/codegen")
+        liftIO (appendFile "daffy-server/codegen/daffy.js" "")
+      True -> pure ()
+    orderOnly ["daffy-server/codegen/daffy.js"]
     files <- getDirectoryFiles "" ["daffy-server/src//*.hs", "daffy-server/app//*.hs"]
-    need ("stack.yaml" : "daffy-server/daffy.cabal" : "daffy-server/codegen/daffy.js" : files)
+    need ("stack.yaml" : "daffy-server/daffy.cabal" : files)
     stack (cmd_ "stack install --fast --flag daffy:development --local-bin-path bin daffy:exe:daffy")
-
-  "bin/Shakefile" %> \_ -> do
-    need ["daffy-shakefile/daffy-shakefile.cabal", "daffy-shakefile/Shakefile.hs"]
-    stack (cmd_ "stack install --local-bin-path bin daffy-shakefile:exe:Shakefile")
 
   "bin/daffy-elm-codegen" %> \_ -> do
     files <- getDirectoryFiles "" ["daffy-elm-codegen/src//*.hs", "daffy-elm-codegen/app//*.hs"]
     need ("stack.yaml" : "daffy-elm-codegen/daffy-elm-codegen.cabal" : files)
     stack (cmd_ "stack install --fast --local-bin-path bin daffy-elm-codegen:exe:daffy-elm-codegen")
+
+  "bin/Shakefile" %> \_ -> do
+    need ["stack.yaml", "daffy-shakefile/daffy-shakefile.cabal", "daffy-shakefile/Shakefile.hs"]
+    stack (cmd_ "stack install --local-bin-path bin daffy-shakefile:exe:Shakefile")
 
   "daffy-client/codegen/DaffyTypes.elm" %> \out -> do
     need ["bin/daffy-elm-codegen"]
@@ -119,5 +120,5 @@ rules stack = do
 
   "daffy-server/codegen/daffy.js" %> \out -> do
     files <- getDirectoryFiles "" ["daffy-client/src//*.elm"]
-    need (".shake/elm-deps.stamp" : "elm-package.json" : "daffy-client/codegen/DaffyTypes.elm" : files)
-    cmd_ ("elm-make --debug daffy-client/src/Main.elm --output=" ++ out)
+    need (".shake/elm-package.json" : "daffy-client/codegen/DaffyTypes.elm" : files)
+    cmd_ ("elm make --debug daffy-client/src/Main.elm --output=" ++ out)
