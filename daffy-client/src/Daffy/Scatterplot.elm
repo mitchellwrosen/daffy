136c5bac00a150a069075b8493a5d531c79ddc7e
Mitchell Rosen <mitchellwrosen@gmail.com>
2018-02-25 23:43:35 -0500

    Draw a gray line thru live bytes svg

--- daffy-client/src/Daffy/Scatterplot.elm
+++ daffy-client/src/Daffy/Scatterplot.elm
@@ -1,14 +1,17 @@
 module Daffy.Scatterplot exposing (Config, svg)
 
 import Daffy.Extent as Extent exposing (Extent)
+import Daffy.List.Extra as List exposing (when)
 import Daffy.Svg
+import Lazy exposing (Lazy, force, lazy)
 import List.Extra as List exposing (last)
 import List.Nonempty as Nonempty exposing (Nonempty(Nonempty))
 import Maybe.Extra as Maybe exposing (unwrap)
 import Svg exposing (Svg)
 import Svg.Attributes
-import Visualization.Axis as VAxis
-import Visualization.Scale as VScale exposing (ContinuousScale)
+import Visualization.Axis as Axis
+import Visualization.Scale as Scale exposing (ContinuousScale)
+import Visualization.Shape as Shape
 
 
 type alias Config a =
@@ -21,6 +24,7 @@ type alias Config a =
         , top : Float
         }
     , extent : Extent -> Extent
+    , line : Bool
     , getX : a -> Float
     , getY : a -> Float
     , getR : a -> Int
@@ -28,29 +32,29 @@ type alias Config a =
 
 
 svg : Config a -> Nonempty a -> Svg msg
-svg { width, height, margin, extent, getX, getY, getR } xs =
+svg { width, height, margin, extent, line, getX, getY, getR } xs =
     let
         { xmin, xmax, ymin, ymax } =
-            extent
-              <| Extent.bufferY 0.05
-              <| Extent.extent { getX = getX, getY = getY } xs
+            extent <|
+                Extent.bufferY 0.05 <|
+                    Extent.extent { getX = getX, getY = getY } xs
 
         xscale : ContinuousScale
         xscale =
-            VScale.linear
+            Scale.linear
                 ( xmin, xmax )
                 ( 0, width - margin.left - margin.right )
 
         yscale : ContinuousScale
         yscale =
-            VScale.linear
+            Scale.linear
                 ( ymin, ymax )
                 ( height - margin.bottom - margin.top, 0 )
 
         xaxis : Svg msg
         xaxis =
-            VAxis.axis
-                { orientation = VAxis.Bottom
+            Axis.axis
+                { orientation = Axis.Bottom
                 , ticks = Nothing
                 , tickFormat = Nothing
                 , tickCount = 10
@@ -62,8 +66,8 @@ svg { width, height, margin, extent, getX, getY, getR } xs =
 
         yaxis : Svg msg
         yaxis =
-            VAxis.axis
-                { orientation = VAxis.Left
+            Axis.axis
+                { orientation = Axis.Left
                 , ticks = Nothing
                 , tickFormat = Nothing
                 , tickCount = 10
@@ -73,16 +77,46 @@ svg { width, height, margin, extent, getX, getY, getR } xs =
                 }
                 yscale
 
-        draw : a -> Svg msg
-        draw x =
-            Svg.g []
-                [ Svg.circle
-                    [ (Svg.Attributes.cx << toString << VScale.convert xscale << getX) x
-                    , (Svg.Attributes.cy << toString << VScale.convert yscale << getY) x
-                    , (Svg.Attributes.r << toString << getR) x
-                    ]
-                    []
-                ]
+        points : List ( Float, Float, Int )
+        points =
+            xs
+                |> Nonempty.toList
+                |> List.map
+                    (\x ->
+                        ( Scale.convert xscale (getX x)
+                        , Scale.convert yscale (getY x)
+                        , getR x
+                        )
+                    )
+
+        circles : List (Svg msg)
+        circles =
+            List.map
+                (\( x, y, r ) ->
+                    Svg.circle
+                        [ Svg.Attributes.cx <| toString x
+                        , Svg.Attributes.cy <| toString y
+                        , Svg.Attributes.r <| toString r
+                        ]
+                        []
+                )
+                points
+
+        path : Lazy (Svg msg)
+        path =
+            lazy
+                (\() ->
+                    Svg.path
+                        [ Svg.Attributes.d
+                            (Shape.line Shape.monotoneInXCurve
+                                (List.map (\( x, y, _ ) -> Just ( x, y )) points)
+                            )
+                        , Svg.Attributes.fill "none"
+                        , Svg.Attributes.strokeWidth "1px"
+                        , Svg.Attributes.stroke "gray"
+                        ]
+                        []
+                )
     in
         Svg.svg
             [ Svg.Attributes.width <| toString width
@@ -92,7 +126,7 @@ svg { width, height, margin, extent, getX, getY, getR } xs =
                 [ Daffy.Svg.transform
                     [ Daffy.Svg.translate margin.left margin.top ]
                 ]
-                [ Svg.g
+                (Svg.g
                     [ Daffy.Svg.transform
                         [ Daffy.Svg.translateY <|
                             height
@@ -101,7 +135,8 @@ svg { width, height, margin, extent, getX, getY, getR } xs =
                         ]
                     ]
                     [ xaxis ]
-                , Svg.g [] [ yaxis ]
-                , Svg.g [] (List.map draw <| Nonempty.toList xs)
-                ]
+                    :: yaxis
+                    :: List.when line (force path)
+                    ++ circles
+                )
             ]
